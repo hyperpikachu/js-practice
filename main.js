221db465359e47e548d277abc20f03c03235a2d3
messi <lsj12323@naver.com>
2022-06-03 00:32:32 +0900

    first

---
+++ main.js
@@ -0,0 +1,67 @@
+function sum(x, y) {
+  return x + y
+}
+const a = sum(1, 3)
+const b = sum(4, 12)
+
+console.log(a)
+console.log(b)
+console.log(a + b)
+
+
+// import random from './getRandom'
+
+// const a = random()
+
+// switch (a) {
+//   case 0:
+//     console.log('a is 0')
+//     break
+//   case 2:
+//     console.log('a is 2')
+//     break
+//   case 4:
+//     console.log('a is 4')
+//     break
+//   default:
+//     console.log('rest...')
+// }
+
+// // if (a === 0) {
+// //   console.log('a is 0')
+// // } else if (a === 2) {
+// //   console.log('a is 2')
+// // } else if (a === 4) {
+// //   console.log('a is 4')
+// // } else {
+// //   console.log('rest...')
+// // }
+// // // // 비교 연산자
+
+// // // const a = 1 === 1
+// // // const b = 'ab' === 'abc'
+// // // const c = false
+
+// // // console.log(a)
+// // // console.log(b)
+// // // console.log(c)
+
+// // // console.log('&&: ', a && b)
+// // // console.log('||: ', a || b || c)
+// // // console.log('!: ', !a)
+
+// // // // 산술 연산자
+
+// // console.log(1 + 2)
+// // console.log(5 - 7)
+// // console.log(3 * 4)
+// // console.log(10 / 2)
+// // console.log(7 % 5)
+
+// // // 할당 연산사
+
+// // let a = 2
+// // // a = a + 1
+// // a %= 1
+
+// // console.log(a)
